b31188750c32ea2c6f2db8004152c17132e43a55
epicurean21 <jm_company@naver.com>
2021-07-30 18:28:38 +0900

    Reactive Streams 기초 4 [Mono]

---
+++ src/main/java/com/example/webflux/ReactiveStudy4/service/MyService.java
@@ -0,0 +1,13 @@
+package com.example.webflux.ReactiveStudy4.service;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyService {
+
+    @Async
+    public String work(String req) {
+        return req + "/asyncwork";
+    }
+}
