58e1877c9db15274f8d23e92abf8701038d2a9d3
epicurean21 <jm_company@naver.com>
2021-11-24 15:13:06 +0900

    fix: RENAME Package [mono]

--- src/main/java/com/example/webflux/mono/controller/TestController.java
+++ src/main/java/com/example/webflux/mono/controller/TestController.java
@@ -1,6 +1,6 @@
-package com.example.webflux.Mono.controller;
+package com.example.webflux.mono.controller;
 
-import com.example.webflux.Mono.service.MyService;
+import com.example.webflux.mono.service.MyService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
